49976efab5b6413b6519ccd44dc330e48ef1d533
Charles Kim <cysk92@gmail.com>
2022-04-28 13:02:14 -0700

    Update example

--- localfail/main.tf
+++ localfail/main.tf
@@ -2,13 +2,6 @@ data "aws_security_group" "sg" {
     name = "test-sg"
 }
 
-locals {
-  ssh_ingress = {
-      "prod": "192.168.1.0/24",
-      "dev": "0.0.0.0/0"
-  }
-}
-
 variable "env" {
     default = "dev"
 }
